ba914a377a38004fdd914243beec411a43d2511f
Devon Wolfkiel <devonwolf@tutanota.com>
2021-02-09 14:54:47 -0800

    Create dropdown component and add basic version to App.js

--- src/App.js
+++ src/App.js
@@ -2,11 +2,13 @@ import './App.css';
 import creatureData from './data/image-data.js';
 import Header from './components/Header.js';
 import ImageList from './components/ImageList.js';
+import Dropdown from './components/Dropdown.js';
 
 function App() {
   return (
     <div className="App">
       <Header />
+      <Dropdown />
       <ImageList creatures={creatureData}/>
     </div>
   );
---
+++ src/components/Dropdown.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react'
+import style from './Dropdown.module.css'
+
+export default class Dropdown extends Component {
+	render() {
+		return (
+			<select>
+				<option>option</option>
+				<option>option</option>
+				<option>option</option>
+				<option>option</option>
+			</select>
+		)
+	}
+}
