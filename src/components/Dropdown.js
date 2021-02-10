4379240e5cf45825595df6edd639d0e9a10d7530
Devon Wolfkiel <devonwolf@tutanota.com>
2021-02-10 14:19:58 -0800

    Clean up dropdown component

--- src/components/Dropdown.js
+++ src/components/Dropdown.js
@@ -5,34 +5,19 @@ import DropdownItem from './DropdownItem.js'
 
 export default class Dropdown extends Component {
 	render() {
-		// const getUniqueListItems = (property) => {
-		// 	let listItems = [];
-		// 	if (property === 'keyword') {
-		// 		for (let i of this.props.options) {
-		// 			if (!listItems.some(n => n === i.keyword)) {
-		// 			listItems.push(i.keyword);
-		// 			}
-		// 		}
-		// 	}
-		// 	else if (property === 'horns') {
-		// 		for (let i of this.props.options) {
-		// 			if (!listItems.some(n => n === i.horns)) {
-		// 				listItems.push(i.horns);
-		// 			}
-		// 		}
-		// 	}
-		// 	return listItems;
-		// }
-
 		return (
 			<label className={style.dropdownLabel}>
+
 				{this.props.label}
+				
 				<select 
 				className={style.dropdown} 
 				value={this.props.keyword} 
 				onChange={this.props.handleChange}>
+
 					{getUniqueListItems(this.props.property, this.props.options).map(
-						n => <DropdownItem displayText={n} key={n}/>)}
+						item => <DropdownItem displayText={item} key={item}/>)}
+
 				</select>
 			</label>
 		)
